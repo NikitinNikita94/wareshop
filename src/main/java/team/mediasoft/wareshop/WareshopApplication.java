1e9d420d106619c2a9f7dbb3df5325a1cdf8f8f1
nikita <7jay94@mail.ru>
2024-03-21 16:52:15 +0200

    init: init project wareshop.

---
+++ src/main/java/team/mediasoft/wareshop/WareshopApplication.java
@@ -0,0 +1,13 @@
+package team.mediasoft.wareshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/mediasoft/wareshop/WareshopApplicationTests.java
@@ -0,0 +1,13 @@
+package team.mediasoft.wareshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
